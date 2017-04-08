109bbee9b1de87a9459f5190f200cd8eba55793a
Brad Christian <bradxn@gmail.com>
2017-04-08 05:25:13 +0000

    added mplayer and airplay starut; switch to pi

--- doorbell.sh
+++ doorbell.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /home/root/projects/doorbell
-./gpio
+
+cd /home/pi/projects/doorbell
+
+mkfifo /tmp/mceiling
+chmod 666 /tmp/mceiling
+mplayer -idle -really-quiet -slave -input file=/tmp/mceiling &
+
+./ssdp &
+./gpio 18 &
+
+../airplay-audio-project/shairport/shairport -a 'Ceiling Speakers' &
